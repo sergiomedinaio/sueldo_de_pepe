259293a0d54e48e1d6e160a15b8ceb4f1443af10
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-11-03 15:45:12 -0300

    feat: add sueldoPresentismo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Gerente pepe = new Gerente();
         System.out.println(pepe.sueldoNeto());
+        System.out.println(pepe.sueldoPresentismo(0));
     }
 }
--- src/Trabajador.java
+++ src/Trabajador.java
@@ -1,3 +1,12 @@
 public abstract class Trabajador {
     public abstract int sueldoNeto ();
+
+    public int sueldoPresentismo() {
+        return 100;
+    }
+    public int sueldoPresentismo(int diasAusentes) {
+        if(diasAusentes == 0) return sueldoPresentismo();
+        if(diasAusentes == 1) return 50;
+        return 0;
+    }
 }
